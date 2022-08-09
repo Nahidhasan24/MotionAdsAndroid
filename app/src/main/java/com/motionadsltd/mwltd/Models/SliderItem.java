7d10a652d4ee84775e3b259acb132adce2b7f4cf
Nahid <nahidhasan24>
2022-08-09 10:24:36 +0600

    change change slider

---
+++ app/src/main/java/com/motionadsltd/mwltd/Models/SliderItem.java
@@ -0,0 +1,29 @@
+package com.motionadsltd.mwltd.Models;
+
+public class SliderItem {
+    private String url,click;
+
+    public SliderItem() {
+    }
+
+    public SliderItem(String url, String click) {
+        this.url = url;
+        this.click = click;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getClick() {
+        return click;
+    }
+
+    public void setClick(String click) {
+        this.click = click;
+    }
+}
