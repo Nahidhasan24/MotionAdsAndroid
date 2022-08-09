0abed2695febf7e33221c6baaa34dd96ada99580
Nahid <nahidhasan24>
2022-08-09 13:35:54 +0600

    change change slider

--- app/src/main/java/com/motionadsltd/mwltd/Activitys/MainActivity.java
+++ app/src/main/java/com/motionadsltd/mwltd/Activitys/MainActivity.java
@@ -1,5 +1,6 @@
 package com.motionadsltd.mwltd.Activitys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -8,16 +9,26 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.motionadsltd.mwltd.Adapters.SliderAdapterExample;
+import com.motionadsltd.mwltd.Models.SliderItem;
 import com.motionadsltd.mwltd.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     FirebaseAuth mAuth;
     DatabaseReference mRef;
+    DatabaseReference mSlider;
     ProgressDialog progressDialog;
+    SliderAdapterExample sliderAdapterExample;
+    ArrayList<SliderItem>sliderItemArrayList=new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         mAuth = FirebaseAuth.getInstance();
         mRef = FirebaseDatabase.getInstance().getReference().child("users");
+        mSlider = FirebaseDatabase.getInstance().getReference().child("slider");
+        // init progress dialog
         progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setCancelable(false);
         progressDialog.setTitle("Loading....");
+        progressDialog.show();
+        loadSlider();
         binding.videoAdsBtn.setOnClickListener(v->{
             startActivity(new Intent(getApplicationContext(), Videoads_Activity.class));
             ///
@@ -78,10 +93,39 @@ public class MainActivity extends AppCompatActivity {
             Intent in=new Intent( MainActivity.this, AddMoneyActivity.class);
             startActivity(in);
         });
+        binding.profileBtn.setOnClickListener(c->{
+            startActivity(new Intent(getApplicationContext(),Profile_Activity.class));
+        });
 
 
 
 
 
+
+    }
+    //getting slider image data from database
+    private void loadSlider() {
+        mSlider.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                progressDialog.dismiss();
+                if (snapshot.exists()){
+                    for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                        SliderItem sliderItem=dataSnapshot.getValue(SliderItem.class);
+                        sliderItemArrayList.add(sliderItem);
+                    }
+                    sliderAdapterExample=new SliderAdapterExample(getApplicationContext());
+                    sliderAdapterExample.renewItems(sliderItemArrayList);
+                    binding.imageSlider.setSliderAdapter(sliderAdapterExample);
+                    binding.imageSlider.startAutoCycle();
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                progressDialog.dismiss();
+            }
+        });
     }
 }
